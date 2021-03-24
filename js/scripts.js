c57e85c1c5bc4499493a4b583783feef158a6622
Cristina Plesa <cristinaplesa8@gmail.com>
2021-03-24 16:20:27 -0700

    Add form subtract in html and js.
    Co-authored-by: Seth Medeiros <Medeirosseth@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-function subtract(number1, number2) {
-  return number1 - number2;
+function subtract(number3, number4) {
+  return number3 - number4;
 }
 
 function multiply(number1, number2) {
@@ -16,11 +16,19 @@ function divide(number1, number2) {
 }
 $(document).ready(function() {
   $("form#add").submit(function(event){
-  event.preventDefault();
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
+    event.preventDefault();
+  const number1 = parseInt($("#input1").val());
+  const number2 = parseInt($("#input2").val());
   const result = add(number1, number2);
   $("#output").text(result);
-
   });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number3 = parseInt($("#input3").val());
+    const number4 = parseInt($("#input4").val());
+    const result = subtract(number3, number4);
+    $("#output1").text(result);
+  });
+
 });
