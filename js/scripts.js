d422eab2fd702fadf5512ca4252c6f39413dd8e1
Cristina Plesa <cristinaplesa8@gmail.com>
2021-03-25 16:34:56 -0700

    Add business and user logic and html layout.
    Co-authored-by: Tori West <victoria_west@ymail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,20 +15,21 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 $(document).ready(function() {
-  $("form#add").submit(function(event){
+  $("form#calculator").submit(function(event){
     event.preventDefault();
-  const number1 = parseInt($("#input1").val());
-  const number2 = parseInt($("#input2").val());
-  const result = add(number1, number2);
-  $("#output").text(result);
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    if (operator==="add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator==="divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
   });
-
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    const number3 = parseInt($("#input3").val());
-    const number4 = parseInt($("#input4").val());
-    const result = subtract(number3, number4);
-    $("#output1").text(result);
-  });
-
+  
 });
